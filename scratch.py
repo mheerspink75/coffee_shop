687fe71eb52cca9e34502632800f750278e647b3
Matt Heerspink <mheerspink75@gmail.com>
2022-03-09 07:45:41 -0600

    updated exception handling

--- scratch.py
+++ scratch.py
@@ -32,114 +32,47 @@ def start_price_list():
     for i in range(len(x)):
         print(f'{y[i]}: {x[i]}')
 
-    return get_size()
+    return order_coffee()
 
 
-def get_size():
-    while len(order) < len(order) + 1:
+def order_coffee():
+    for i in range(len(x)):
+        while len(order) < len(order) + 1:
+            next_choice = x[i]
+            choices = [key for key in next_choice.keys()]
+            price = [value for value in next_choice.values()]
 
-        next_choice = x[0]
-        choices = [key for key in next_choice.keys()]
-        price = [value for value in next_choice.values()]
+            print(f'\nChoose a {y[i]}...\n')
+            for j in range(len(x[i])):
+                print(f'{j+1} - {choices[j]}')
+            print(f'{len(x[i]) + 1} - quit\n')
 
-        print(f'\nChoose a {y[0]}...\n')
-        for i in range(len(x[0])):
-            print(f'{i+1} - {choices[i]}')
-        print(f'{len(x[0]) + 1} - quit\n')
-
-        try:
-            s_input = int(input())
-            if s_input == len(next_choice) + 1:
-                message = f'\nYou chose: {s_input} - Quit: Goodbye!'
-                return print(message)
-            else:
-                message = f'{s_input} - {choices[s_input - 1]}'
-                user_choice = {
-                    f'{choices[s_input - 1]}': price[s_input - 1]}
-                order.update(user_choice)
-                print(
-                    f'\nYou chose: {message}\n${round(float(price[s_input - 1]), 2)} added to order_total')
-        except:
-            print('\nPlease enter a valid choice...')
-            continue
-        if s_input < 1 or s_input > len(x[0]) + 1:
-            print('\nEnter a value between 1 and 4')
-            continue
-        break
-
-    return get_coffee()
-
-
-def get_coffee():
-    while len(order) < len(order) + 1:
-        next_choice = x[1]
-        choices = [key for key in next_choice.keys()]
-        price = [value for value in next_choice.values()]
-
-        print(f'\nChoose a {y[1]}...\n')
-        for j in range(len(x[1])):
-            print(f'{j+1} - {choices[j]}')
-        print(f'{len(x[1]) + 1} - quit\n')
-
-        try:
-            s_input = int(input())
-            if s_input == len(next_choice) + 1:
-                message = f'You chose: {s_input} - Quit: Goodbye!'
-                return print(message)
-            else:
-                message = f'{s_input} - {choices[s_input - 1]}'
-                user_choice = {
-                    f'{choices[s_input - 1]}': price[s_input - 1]}
-                order.update(user_choice)
-                print(
-                    f'\nYou chose: {message}\n${round(float(price[s_input - 1]), 2)} added to order_total\n')
-        except:
-            print('Please enter a valid choice...')
-            continue
-        if s_input < 1 or s_input > len(x[1]) + 1:
-            print('Enter a value between 1 and 4')
-            continue
-        break
-
-    return get_flavor()
-
-
-def get_flavor():
-    while len(order) < len(order) + 1:
-        next_choice = x[2]
-        choices = [key for key in next_choice.keys()]
-        price = [value for value in next_choice.values()]
-
-        print(f'\nChoose a {y[2]}...\n')
-        for k in range(len(x[2])):
-            print(f'{k+1} - {choices[k]}')
-        print(f'{len(x[2]) + 1} - quit\n')
-
-        try:
-            s_input = int(input())
-            if s_input == len(next_choice) + 1:
-                message = f'You chose: {s_input} - Quit: Goodbye!'
-                return print(message)
-            else:
-                message = f'{s_input} - {choices[s_input - 1]}'
-                user_choice = {
-                    f'{choices[s_input - 1]}': price[s_input - 1]}
-                order.update(user_choice)
-                print(
-                    f'\nYou chose: {message}\n${round(float(price[s_input - 1]), 2)} added to order_total\n')
-        except:
-            print('Please enter a valid choice...')
-            continue
-        if s_input < 1 or s_input > len(x[1]) + 1:
-            print('Enter a value between 1 and 4')
-            continue
-        break
+            try:
+                s_input = int(input())
+                if s_input == len(next_choice) + 1:
+                    message = f'\nYou chose: {s_input} - Quit: Goodbye!'
+                    return print(message)
+                else:
+                    if s_input !=0:
+                        message = f'{s_input} - {choices[s_input - 1]}'
+                        user_choice = {
+                            f'{choices[s_input - 1]}': price[s_input - 1]}
+                        order.update(user_choice)
+                        print(
+                            f'\nYou chose: {message}\n${round(float(price[s_input - 1]), 2)} added to order_total')
+            except:
+                print('\nPlease enter a valid choice...')
+                continue
+            if s_input == 0:
+                print(f'\nEnter a value between 1 and 4')
+                continue
+            break
 
     return calculate_total()
 
 
 def calculate_total():
-    print(f'Coffee Order: {order}\n')
+    print(f'\nCoffee Order: {order}\n')
     total = round(float(sum(order.values())), 2)
     tip_amount = round(float(total * tip), 2)
 
