20117b3f9080c1985f8b8e82bf16500f8bf62d33
Matt Heerspink <mheerspink75@gmail.com>
2022-03-11 12:27:39 -0600

    updated comments scratch.py

--- scratch.py
+++ scratch.py
@@ -1,4 +1,9 @@
-# Coffee Shop
+################################################
+################# COFFEE SHOP ##################
+##### Matt Heerspink ###########################
+##### 03/11/2022 ###############################
+################################################
+
 coffee_sizes = {
     'small': 2,
     'medium': 3,
@@ -28,7 +33,9 @@ order = {}
 orders = []
 totals = []
 
+
 def start_price_list():
+    ##### start the price list and display results ###########################
     print('\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ COFFEE SHOP ////////////////////')
     print('\n--------------------PRICE LIST--------------------\n')
     for i in range(len(x)):
@@ -40,18 +47,19 @@ def start_price_list():
 
 
 def order_coffee():
+    ### order coffee - loop choices and prices ##########
     for i in range(len(x)):
         next_choice = x[i]
         choices = [key for key in next_choice.keys()]
         price = [value for value in next_choice.values()]
-
+        ## display multiple choices and prices ##########
         while True:
             print(f'\nChoose a {y[i]}...\n')
-
+            ## display choice and price lists ###########
             for j in range(len(next_choice)):
                 print(f'{j+1} - {choices[j]} --> ${float(price[j])}0')
             print(f'{len(next_choice) + 1} - quit\n')
-
+            ## handle input exceptions ##################
             try:
                 s_input = int(input())
                 if s_input == len(next_choice) + 1:
@@ -59,12 +67,14 @@ def order_coffee():
 
                     return print(message)
                 else:
+                    ## store orders form input ##########
                     if s_input != 0:
                         message = f'{s_input} - {choices[s_input - 1]}'
                         user_choice = {
                             f'{choices[s_input - 1]}': price[s_input - 1]}
                         order.update(user_choice)
-                        totals.append(price[s_input -1])
+                        totals.append(price[s_input - 1])
+                        ## display confirmation ######## 
                         print(
                             f'\nYou chose: {message}\n${float(price[s_input - 1])}0 added to order_total')
                         break
@@ -75,7 +85,6 @@ def order_coffee():
                 print(f'\nEnter a value between 1 and 4')
                 continue
             break
-        
 
     return calculate_total()
 
@@ -84,12 +93,12 @@ def calculate_total():
     coffeez = 'COFFEE'
     if len(orders) != 0:
         coffeez = 'COFFEES'
-
-    orders.append([order])    
+    ## calculate orders ##
+    orders.append([order])
     coffee_total = float(sum(totals))
     tip_amount = round(coffee_total * tip, 2)
     total = round(coffee_total + tip_amount, 2)
-    
+    ## display confirmation messages #################################################
     order_summary = f'Coffee: ${coffee_total}0\nTip: ${tip_amount}\n\nTotal: ${total}'
     print('\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ COFFEE SHOP ////////////////////')
     print(f'\n------------YOU ORDERED {len(orders)} {coffeez}----------------')
@@ -97,7 +106,7 @@ def calculate_total():
     print(f'{order_summary}\n')
     print('-------------ORDER ANOTHER COFFEE?---------------')
     print('\n   ||||||||||||| COFFEE SHOP |||||||||||||||||')
-    
+
     return order_coffee()
 
 
